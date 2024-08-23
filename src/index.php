0ea2aac1d099f954a1b84b5fc678fc02b7326f81
Jakub Dvořáček <45931076+dvoracek-jakub@users.noreply.github.com>
2024-08-23 15:10:33 +0200

    PHP Dockerfile & configs; Postgres service

--- src/index.php
+++ src/index.php
@@ -1,3 +1,7 @@
 <?php
 
-echo "It works!";
+$dbconn = pg_connect("host=postgres port=5432 dbname=postgres user=root password=secret456");
+echo $dbconn ? "postgre connected" : "postgre connection failed";
+pg_close($dbconn);
+
+phpinfo();
