c65aaf6a635a4ba485c0a8fd0e7b687ad6f4f0f7
Jakub Dvořáček <45931076+dvoracek-jakub@users.noreply.github.com>
2024-08-26 16:53:32 +0200

    DB Evolution

--- docker/postgres/dbevo.php
+++ docker/postgres/dbevo.php
@@ -2,45 +2,80 @@
 
 /** Database evolution script */
 
-$rootUser = getenv('POSTGRES_USER');
-$rootPass = getenv('POSTGRES_PASSWORD');
-
+$log = [];
 $storeDb = 'academistore';
+$saveLog = getenv('DBEVO_LOG');
 $storeUser = getenv('POSTGRES_STORE_USER');
 $storePass = getenv('POSTGRES_STORE_PASSWORD');
+$sqlEvoPath = getenv('DBEVO_SQL_PATH');
 
-
-$dbconn = pg_connect("host=postgres port=5432 dbname=postgres user=$rootUser password=$rootPass");
-$checkDbQuery = "SELECT 1 FROM pg_database WHERE datname = '$storeDb'";
-$result = pg_query($dbconn, $checkDbQuery);
-
+$db = getDbConnection('postgres');
+$result = pg_query($db, "SELECT 1 FROM pg_database WHERE datname = '$storeDb'");
 
 // Database initialization
 if (pg_num_rows($result) == 0) {
-
+	$log[] = 'Creating database: ';
 	$createDbQuery = "CREATE DATABASE $storeDb";
-	$result = pg_query($dbconn, $createDbQuery);
+	$result = pg_query($db, $createDbQuery);
 
 	if ($result) {
-		$dbconn = pg_connect("host=postgres port=5432 dbname=$storeDb user=$rootUser password=$rootPass");
+		$log[] = 'Success!';
+
+		$asdb = getDbConnection($storeDb);
 		$result = pg_query(
-			$dbconn, "
+			$asdb, "
 			CREATE SCHEMA system;
 			CREATE TABLE system.db(version SMALLINT DEFAULT 0);
 			INSERT INTO system.db(version) VALUES(0);"
 		);
 
+		$log[] = 'System schema ' . ($result ? 'created' : 'NOT created: ') . pg_last_error($db);
+
 		$result = pg_query(
-			$dbconn, "
+			$db, "
 			CREATE USER $storeUser WITH ENCRYPTED PASSWORD '$storePass';
 			ALTER DATABASE $storeDb OWNER TO $storeUser;"
 		);
+		$log[] = 'User and permissions ' . ($result ? 'created' : 'NOT created: ') . pg_last_error($db);
+	} else {
+		$log[] = 'Fail! ' . pg_last_error($db);
 	}
+} else {
+	$log[] = 'Database exists';
 }
 
 
-// @TODO logika pro iteraci sql scriptu
-// Ano, opravdu tady. Bude se tudiz provadet i pri uplne prvnim spusteni  - po initu db
+// Database evolution
+$sqlFiles = glob($sqlEvoPath . '*.sql');
+if ($sqlFiles !== false && count($sqlFiles) > 0) {
+	$db = getDbConnection($storeDb);
+	foreach ($sqlFiles as $file) {
+		$evoIter = (int) basename($file, '.sql');
+		$result = pg_query($db, "SELECT version FROM system.db WHERE version < $evoIter");
 
+		if ($result && pg_num_rows($result) > 0) {
+			$log[] = "Found file to import: $file";
+			$sql = file_get_contents($file);
 
-pg_close($dbconn);
+			if ($sql !== false) {
+				$result = pg_query($db, $sql);
+				$log[] = 'Query was ' . ($result ? 'executed' : 'NOT executed: ') . pg_last_error($db);
+			} else {
+				$log[] = 'Error loading file!';
+			}
+		}
+	}
+}
+
+pg_close($db);
+
+if ($saveLog) {
+	file_put_contents(getenv('DBEVO_LOGFILE'), print_r($log, true));
+}
+
+function getDbConnection(string $dbName)
+{
+	$rootUser = getenv('POSTGRES_USER');
+	$rootPass = getenv('POSTGRES_PASSWORD');
+	return pg_connect("host=postgres port=5432 dbname=$dbName user=$rootUser password=$rootPass");
+}
